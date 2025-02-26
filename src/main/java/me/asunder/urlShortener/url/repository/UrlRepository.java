876531968a1006d942f6a95792dbc3a1db592751
glass <88633725+glass-source@users.noreply.github.com>
2025-02-26 14:45:46 -0400

    feat: cleanup of old project structure

--- src/main/java/me/asunder/urlShortener/url/repository/UrlRepository.java
+++
@@ -1,6 +0,0 @@
-package me.asunder.urlShortener.url.repository;
-
-import me.asunder.urlShortener.url.model.ShortUrl;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UrlRepository extends MongoRepository<ShortUrl, String> {}
